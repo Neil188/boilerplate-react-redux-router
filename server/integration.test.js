81281dd7eb4c19cc4eac9ae09c90932fd26208dd
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-10-09 19:37:01 +0100

    fix: server integration test

--- server/integration.test.js
+++ server/integration.test.js
@@ -4,7 +4,7 @@ const app = require('./app');
 test('should pass integration tests', done => {
     request(app)
         .get('/')
-        .expect('Content-Type', 'text/html; charset=UTF-8')
+        .expect('Content-Type', /html/)
         .expect(200)
         .end((err) => {
             if (err) throw done(err);
